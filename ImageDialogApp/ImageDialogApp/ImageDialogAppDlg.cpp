add6035722a668d8c4802a337db223d5b043c915
programmiy <46128487+programmiy@users.noreply.github.com>
2024-12-24 23:56:31 +0900

    중간 백업
    다소 부족한 완성도 
    개념 이해는 했지만 open에서 막힘
    rect --> image로 갈계획

---
+++ ImageDialogApp/ImageDialogApp/ImageDialogAppDlg.cpp
@@ -0,0 +1,321 @@
+﻿// ImageDialogAppDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ImageDialogApp.h"
+#include "ImageDialogAppDlg.h"
+#include "afxdialogex.h"
+#include <thread>
+#include <random>
+#include <direct.h>
+#include <errno.h>
+#include <ctime>
+#include <gdiplus.h>
+using namespace Gdiplus;
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+BEGIN_MESSAGE_MAP(CImageDialogAppDlg, CDialogEx)
+    ON_BN_CLICKED(IDC_BUTTON_DRAW, &CImageDialogAppDlg::OnBnClickedButtonDraw)
+    ON_BN_CLICKED(IDC_BUTTON_ACTION, &CImageDialogAppDlg::OnBnClickedButtonAction)
+    ON_BN_CLICKED(IDC_BUTTON_OPEN, &CImageDialogAppDlg::OnBnClickedButtonOpen)
+    ON_WM_SYSCOMMAND()
+    ON_WM_PAINT()
+    
+    ON_BN_CLICKED(IDOK, &CImageDialogAppDlg::OnBnClickedOk)
+    ON_EN_CHANGE(IDC_EDIT_X1, &CImageDialogAppDlg::OnEnChangeEditX1)
+END_MESSAGE_MAP()
+
+CImageDialogAppDlg::CImageDialogAppDlg(CWnd* pParent /*=nullptr*/)
+    : CDialogEx(IDD_IMAGEDIALOGAPP_DIALOG, pParent), m_radius(0), m_x(0), m_y(0)
+{
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CImageDialogAppDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CDialogEx::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_EDIT_X1, m_editX1);
+    DDX_Control(pDX, IDC_EDIT_Y1, m_editY1);
+    DDX_Control(pDX, IDC_EDIT_X2, m_editX2);
+    DDX_Control(pDX, IDC_EDIT_Y2, m_editY2);
+    DDX_Control(pDX, IDC_STATIC_IMAGE, m_imageCtrl);
+}
+
+ULONG_PTR gdiplusToken;
+
+BOOL CImageDialogAppDlg::OnInitDialog()
+{
+    CDialogEx::OnInitDialog();
+
+    GdiplusStartupInput gdiplusStartupInput;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+
+    // 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+    CMenu* pSysMenu = GetSystemMenu(FALSE);
+    if (pSysMenu != nullptr)
+    {
+        BOOL bNameValid;
+        CString strAboutMenu;
+        bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+        ASSERT(bNameValid);
+        if (!strAboutMenu.IsEmpty())
+        {
+            pSysMenu->AppendMenu(MF_SEPARATOR);
+            pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+        }
+    }
+
+    SetIcon(m_hIcon, TRUE);         // 큰 아이콘을 설정합니다.
+    SetIcon(m_hIcon, FALSE);        // 작은 아이콘을 설정합니다.
+
+    // 컨트롤 초기화
+    if (!m_editX1.SubclassDlgItem(IDC_EDIT_X1, this) ||
+        !m_editY1.SubclassDlgItem(IDC_EDIT_Y1, this) ||
+        !m_editX2.SubclassDlgItem(IDC_EDIT_X2, this) ||
+        !m_editY2.SubclassDlgItem(IDC_EDIT_Y2, this) ||
+        !m_imageCtrl.SubclassDlgItem(IDC_STATIC_IMAGE, this))
+    {
+        AfxMessageBox(_T("컨트롤 초기화 실패"));
+        return FALSE;
+    }
+
+
+    // 레이블 초기화
+    SetDlgItemText(IDC_STATIC_X1_LABEL, _T("X1:"));
+    SetDlgItemText(IDC_STATIC_Y1_LABEL, _T("Y1:"));
+    SetDlgItemText(IDC_STATIC_X2_LABEL, _T("X2:"));
+    SetDlgItemText(IDC_STATIC_Y2_LABEL, _T("Y2:"));
+
+    // 그리기 영역 초기화
+    CRect rect;
+    m_imageCtrl.GetClientRect(&rect);
+    CDC* pDC = m_imageCtrl.GetDC();
+    pDC->FillSolidRect(&rect, RGB(0, 0, 0)); // 바탕색 설정
+    pDC->Rectangle(&rect); // 테두리 설정
+    m_imageCtrl.ReleaseDC(pDC);
+
+    return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+CImageDialogAppDlg::~CImageDialogAppDlg()
+{
+    CDialogEx::OnDestroy();
+
+    // GDI+ 종료
+    GdiplusShutdown(gdiplusToken);
+}
+
+void CImageDialogAppDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+    if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+    {
+        CDialogEx dlgAbout(IDD_ABOUTBOX);
+        dlgAbout.DoModal();
+    }
+    else
+    {
+        CDialogEx::OnSysCommand(nID, lParam);
+    }
+}
+
+void CImageDialogAppDlg::OnBnClickedButtonDraw()
+{
+    CString strX1, strY1;
+    m_editX1.GetWindowText(strX1);
+    m_editY1.GetWindowText(strY1);
+
+    int x1 = _ttoi(strX1);
+    int y1 = _ttoi(strY1);
+
+    // 랜덤한 반지름 생성
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(10, 100); // 반지름 범위 설정
+    int radius = dis(gen);
+
+    // 그리기 영역을 가져옵니다.
+    CRect rect;
+    m_imageCtrl.GetClientRect(&rect);
+    CDC* pDC = m_imageCtrl.GetDC();
+
+    // 그리기 영역 초기화 (검은색 배경)
+    pDC->FillSolidRect(&rect, RGB(0, 0, 0));
+
+    // 원 그리기 (하얀색 내부, 색 테두리)
+    CPen pen(PS_SOLID, 1, RGB(255, 255, 255));
+    CBrush brush(RGB(255, 255, 255));
+    CPen* pOldPen = pDC->SelectObject(&pen);
+    CBrush* pOldBrush = pDC->SelectObject(&brush);
+    pDC->Ellipse(x1 - radius, y1 - radius, x1 + radius, y1 + radius);
+    pDC->SelectObject(pOldPen);
+    pDC->SelectObject(pOldBrush);
+
+    // DC를 해제합니다.
+    m_imageCtrl.ReleaseDC(pDC);
+}
+
+void CImageDialogAppDlg::OnBnClickedButtonAction()
+{
+    CString strX1, strY1, strX2, strY2;
+    m_editX1.GetWindowText(strX1);
+    m_editY1.GetWindowText(strY1);
+    m_editX2.GetWindowText(strX2);
+    m_editY2.GetWindowText(strY2);
+
+    int x1 = _ttoi(strX1);
+    int y1 = _ttoi(strY1);
+    int x2 = _ttoi(strX2);
+    int y2 = _ttoi(strY2);
+
+    // 별도의 스레드에서 작업을 수행합니다.
+    std::thread([=]() {
+        // 디렉토리 생성
+        if (_mkdir("image") == -1)
+        {
+            if (errno != EEXIST)
+            {
+                AfxMessageBox(_T("디렉토리 생성 실패"));
+                return;
+            }
+        }
+
+        // 이동 간격 설정
+        int step = 10;
+
+        // 이동하면서 원을 그립니다.
+        for (int x = x1, y = y1; x <= x2 && y <= y2; x += step, y += step)
+        {
+            // 그리기 영역을 가져옵니다.
+            CRect rect;
+            m_imageCtrl.GetClientRect(&rect);
+            CDC* pDC = m_imageCtrl.GetDC();
+
+            // 그리기 영역 초기화 (검은색 배경)
+            pDC->FillSolidRect(&rect, RGB(0, 0, 0));
+
+            // 원 그리기 (하얀색 내부, 검은색 테두리)
+            CPen pen(PS_SOLID, 1, RGB(0, 0, 0));
+            CBrush brush(RGB(255, 255, 255));
+            CPen* pOldPen = pDC->SelectObject(&pen);
+            CBrush* pOldBrush = pDC->SelectObject(&brush);
+            pDC->Ellipse(x - 50, y - 50, x + 50, y + 50);
+            pDC->SelectObject(pOldPen);
+            pDC->SelectObject(pOldBrush);
+
+            // 현재 화면을 캡처하여 파일로 저장
+            CImage image;
+            image.Create(rect.Width(), rect.Height(), 32);
+            CDC* pImageDC = CDC::FromHandle(image.GetDC());
+            pImageDC->BitBlt(0, 0, rect.Width(), rect.Height(), pDC, 0, 0, SRCCOPY);
+            image.ReleaseDC();
+
+            // 파일 이름 생성 (타임스탬프 포함)
+            CString fileName;
+            CTime time = CTime::GetCurrentTime();
+            fileName.Format(_T("image\\image_%04d%02d%02d%02d%02d%02d_%03d.jpg"),
+                            time.GetYear(), time.GetMonth(), time.GetDay(),
+                            time.GetHour(), time.GetMinute(), time.GetSecond(), x);
+
+            // 파일 저장
+            image.Save(fileName, Gdiplus::ImageFormatJPEG);
+
+            // DC를 해제합니다.
+            m_imageCtrl.ReleaseDC(pDC);
+
+            // 잠시 대기 (애니메이션 효과)
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }).detach();
+}
+
+void CImageDialogAppDlg::OnBnClickedButtonOpen()
+{
+    CFileDialog dlg(TRUE, _T("Image Files"), NULL, OFN_FILEMUSTEXIST, _T("Images|*.bmp;*.jpg;*.jpeg|All Files|*.*||"));
+    if (dlg.DoModal() == IDOK)
+    {
+        CString path = dlg.GetPathName();
+
+        // 이미지 로드
+        CImage image;
+        HRESULT hr = image.Load(path);
+        if (FAILED(hr))
+        {
+            AfxMessageBox(_T("이미지를 로드할 수 없습니다."));
+            return;
+        }
+
+        // 그리기 영역을 가져옵니다.
+        CRect rect;
+        m_imageCtrl.GetClientRect(&rect);
+
+        // DC(Device Context)를 가져옵니다.
+        CDC* pDC = m_imageCtrl.GetDC();
+
+        // 화면을 지웁니다.
+        pDC->FillSolidRect(&rect, RGB(240, 240, 240)); // 바탕색 설정
+
+        // 이미지 표시
+        image.Draw(pDC->m_hDC, rect);
+
+        // 원 중심의 X 표시 및 좌표 출력
+        int x = m_x;
+        int y = m_y;
+        pDC->MoveTo(x - 10, y);
+        pDC->LineTo(x + 10, y);
+        pDC->MoveTo(x, y - 10);
+        pDC->LineTo(x, y + 10);
+
+        CString strCoords;
+        strCoords.Format(_T("(%d, %d)"), x, y);
+        pDC->TextOutW(x + 15, y, strCoords);
+
+        // DC를 해제합니다.
+        m_imageCtrl.ReleaseDC(pDC);
+    }
+}
+
+void CImageDialogAppDlg::OnPaint()
+{
+    if (IsIconic())
+    {
+        CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+        SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+        // 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+        int cxIcon = GetSystemMetrics(SM_CXICON);
+        int cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
+
+        // 아이콘을 그립니다.
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CDialogEx::OnPaint();
+    }
+}
+
+void CImageDialogAppDlg::OnBnClickedOk()
+{
+    // TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+    CDialogEx::OnOK();
+}
+
+void CImageDialogAppDlg::OnEnChangeEditX1()
+{
+    // TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+    // CDialogEx::OnInitDialog() 함수를 재지정 
+    //하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+    // ENM_CHANGE가 있으면 마스크에 ORed를 플래그합니다.
+
+    // TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ ImageDialogApp/ImageDialogApp/ImageDialogAppDlg.h
@@ -0,0 +1,53 @@
+﻿
+// ImageDialogAppDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CImageDialogAppDlg 대화 상자
+class CImageDialogAppDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CImageDialogAppDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	afx_msg void OnBnClickedButtonDraw();
+	afx_msg void OnBnClickedButtonAction();
+	afx_msg void OnBnClickedButtonOpen();
+	virtual BOOL OnInitDialog();
+	void OnInitDialogCustom();
+	void DrawCircleOnBlack(CImage& img, int x, int y, int radius);    
+
+    virtual ~CImageDialogAppDlg();                  // 소멸자 추가
+	// 대화 상자 데이터입니다.
+	#ifdef AFX_DESIGN_TIME
+		enum { IDD = IDD_IMAGEDIALOGAPP_DIALOG };
+	#endif
+private:
+    int m_x;
+	CImage m_offScreen; // 오프스크린 이미지
+    int m_y;
+
+    int m_radius; // 반지름을 저장하는 멤버 변수
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+
+	// 구현입니다.
+	protected:
+		HICON m_hIcon;
+		CEdit m_editX1;
+		CEdit m_editY1;
+		CEdit m_editX2;
+		CEdit m_editY2;
+		CStatic m_imageCtrl;
+		// 생성된 메시지 맵 함수
+
+		DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnEnChangeEditX1();
+};
